b6a809c1e918a5023aa393fb47cadc94691d2892
Sang-Mini <sangmin3062@gmail.com>
2023-09-14 16:38:52 +0900

    Assets 이미지 파일 업로드, View 폴더 생성, HomeView 생성

---
+++ GestureCard/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  GestureCard
+//
+//  Created by 유상민 on 2023/09/14.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
