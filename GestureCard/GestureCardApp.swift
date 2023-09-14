b17946faca163e22003b8998228d1618d982571d
Sang-Mini <sangmin3062@gmail.com>
2023-09-14 16:33:59 +0900

    Initial Commit

---
+++ GestureCard/GestureCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureCardApp.swift
+//  GestureCard
+//
+//  Created by 유상민 on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct GestureCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
